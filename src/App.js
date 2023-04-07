0bf7691b288020e0fe36504d7f45521fe7db6a71
Shravani K <123314193+Shravani080@users.noreply.github.com>
2023-04-07 12:12:20 +0530

    Update App.js
    just removed unwanted text

--- src/App.js
+++ src/App.js
@@ -1,77 +1,41 @@
-import React from 'react'
-import ToDo from './ToDo/ToDo'
+import { Routes, Route } from "react-router-dom";
+import Dashboard from "./Components/Dashboard/Dashboard";
+import LoginPage from "./Components/LoginPage/LoginPage";
+import './App.css';
+import UserInformation from "./Components/UserInformation/UserInformation";
+import Header from "./Components/Header/Header";
+import { useEffect, useState } from "react";
+import { useLocation } from 'react-router-dom';
+
 
 function App() {
-  return (
-    <div>
-      <ToDo/>
-    </div>
-  )
-}
+  const [display, setDisplay] = useState(true);
+  const location = useLocation();
 
-export default App
-
-
-
-
-
-
-
-
-
-
-
-
-
-// import { Routes, Route } from "react-router-dom";
-// import Dashboard from "./Components/Dashboard/Dashboard";
-// import LoginPage from "./Components/LoginPage/LoginPage";
-// import './App.css';
-// import UserInformation from "./Components/UserInformation/UserInformation";
-// import Header from "./Components/Header/Header";
-// import { useEffect, useState } from "react";
-// import { useLocation } from 'react-router-dom';
-
-
-// function App() {
-//   const [display, setDisplay] = useState(true);
-//   const location = useLocation();
-
-//   useEffect(()=>{
+  useEffect(()=>{
     
-//     if(location.pathname === "/loginpage" || location.pathname === "/" ){
-//       setDisplay(false)
-//     }else{
-//       setDisplay(true)
-//     }
-//   },[location.pathname])
+    if(location.pathname === "/loginpage" || location.pathname === "/" ){
+      setDisplay(false)
+    }else{
+      setDisplay(true)
+    }
+  },[location.pathname])
  
 
-//   return (
-//     <div className="body">
-//       {display && <Header />}
-//       <Routes>
-//         <Route path="/" element={<LoginPage />} />
-//         <Route path="/loginpage" element={<LoginPage />} />
-//         <Route path="/dashboard" element={<Dashboard />} />
-//         <Route path="/userinformation" element={<UserInformation />} />
-//       </Routes>
-//     </div>
-//   );
-// }
-// export default App;
+  return (
+    <div className="body">
+      {display && <Header />}
+      <Routes>
+        <Route path="/" element={<LoginPage />} />
+        <Route path="/loginpage" element={<LoginPage />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/userinformation" element={<UserInformation />} />
+      </Routes>
+    </div>
+  );
+}
+export default App;
 
 
 
 
-
-// {todos.map((item, index) => (
-//   <li>
-//     <input
-//       type="text"
-//       value={item}
-//       onChange={(e) => (todos[index] = e.target.value)}
-//     />
-//     <button onClick={() => todos.splice(index, 1)}>Delete</button>
-//   </li>
-// ))}
