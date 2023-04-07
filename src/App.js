5c4e5e96f282de1c6844340c91a2c396f70647d2
Shravani K <123314193+Shravani080@users.noreply.github.com>
2023-04-07 12:12:53 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,40 +1,40 @@
-// import { Routes, Route } from "react-router-dom";
-// import Dashboard from "./Components/Dashboard/Dashboard";
-// import LoginPage from "./Components/LoginPage/LoginPage";
-// import './App.css';
-// import UserInformation from "./Components/UserInformation/UserInformation";
-// import Header from "./Components/Header/Header";
-// import { useEffect, useState } from "react";
-// import { useLocation } from 'react-router-dom';
+import { Routes, Route } from "react-router-dom";
+import Dashboard from "./Components/Dashboard/Dashboard";
+import LoginPage from "./Components/LoginPage/LoginPage";
+import './App.css';
+import UserInformation from "./Components/UserInformation/UserInformation";
+import Header from "./Components/Header/Header";
+import { useEffect, useState } from "react";
+import { useLocation } from 'react-router-dom';
 
 
-// function App() {
-//   const [display, setDisplay] = useState(true);
-//   const location = useLocation();
+function App() {
+  const [display, setDisplay] = useState(true);
+  const location = useLocation();
 
-//   useEffect(()=>{
+  useEffect(()=>{
     
-//     if(location.pathname === "/loginpage" || location.pathname === "/" ){
-//       setDisplay(false)
-//     }else{
-//       setDisplay(true)
-//     }
-//   },[location.pathname])
+    if(location.pathname === "/loginpage" || location.pathname === "/" ){
+      setDisplay(false)
+    }else{
+      setDisplay(true)
+    }
+  },[location.pathname])
  
 
-//   return (
-//     <div className="body">
-//       {display && <Header />}
-//       <Routes>
-//         <Route path="/" element={<LoginPage />} />
-//         <Route path="/loginpage" element={<LoginPage />} />
-//         <Route path="/dashboard" element={<Dashboard />} />
-//         <Route path="/userinformation" element={<UserInformation />} />
-//       </Routes>
-//     </div>
-//   );
-// }
-// export default App;
+  return (
+    <div className="body">
+      {display && <Header />}
+      <Routes>
+        <Route path="/" element={<LoginPage />} />
+        <Route path="/loginpage" element={<LoginPage />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/userinformation" element={<UserInformation />} />
+      </Routes>
+    </div>
+  );
+}
+export default App;
 
 
 
